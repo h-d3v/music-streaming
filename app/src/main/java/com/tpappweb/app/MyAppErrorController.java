269d2ffa4455e5f776852aa0c5b6ef2a263e168a
Hedi <1756859@crosemont.qc.ca>
2020-05-24 01:47:39 -0400

    Ajout de gestion des erreur minimale dans l'error controller

--- app/src/main/java/com/tpappweb/app/MyAppErrorController.java
+++ app/src/main/java/com/tpappweb/app/MyAppErrorController.java
@@ -1,4 +1,24 @@
 package com.tpappweb.app;
 
-public class MyAppErrorController {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class MyAppErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpSession httpSession) {
+        if(httpSession.getAttribute("utilisateurConnecte")==null){
+            return "index";
+        }
+        else return "player";
+    }
+
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
 }
